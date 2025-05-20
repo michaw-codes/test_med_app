802b0198375dcab7e1fa461b080949341ed6721f
michaw-codes <111310362+michaw-codes@users.noreply.github.com>
2025-05-20 16:26:34 -0400

    feat(Login): add login logic

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 import './Login.css';
 
-function Login() {
+const Login = () => {
+      // State variables for email and password
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState('');
+    // Get navigation function from react-router-dom
+    const navigate = useNavigate();
+    // Check if user is already authenticated, then redirect to home page
+    useEffect(() => {
+        if (sessionStorage.getItem("auth-token")) {
+        navigate("/");
+        }
+    }, []);
+    // Function to handle login form submission
+    const login = async (e) => {
+        e.preventDefault();
+        // Send a POST request to the login API endpoint
+        const res = await fetch(`${API_URL}/api/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        }),
+        });
+        // Parse the response JSON
+        const json = await res.json();
+        if (json.authtoken) {
+        // If authentication token is received, store it in session storage
+        sessionStorage.setItem('auth-token', json.authtoken);
+        sessionStorage.setItem('email', email);
+        // Redirect to home page and reload the window
+        navigate('/');
+        window.location.reload();
+        } else {
+        // Handle errors if authentication fails
+        if (json.errors) {
+            for (const error of json.errors) {
+            alert(error.msg);
+            }
+        } else {
+            alert(json.error);
+        }
+        }
+    };
     return (
       <div className="Login">
             <div className="container">
@@ -14,10 +62,12 @@ function Login() {
                     <a href="../Sign_Up/Sign_Up.html">Sign Up Here</a>
                 </div>
                 <div className="login-form">
-                    <form>
+                    <form onSubmit={login}>
                         <div className="form-group">
-                            <label for="email">Email</label>
+                            <label htmlFor="email">Email</label>
                             <input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 type="email"
                                 name="email"
                                 id="email"
@@ -28,8 +78,10 @@ function Login() {
                             />
                         </div>
                         <div className="form-group">
-                            <label for="password">Password</label>
+                            <label htmlFor="password">Password</label>
                             <input
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 type="password"
                                 name="password"
                                 id="password"
